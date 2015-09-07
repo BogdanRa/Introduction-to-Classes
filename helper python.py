1205bf4d4ebdad458e0364c051aa8ce57885271f
Bohdan <radchenkobodya@gmail.com>
2015-09-07 12:38:24 +0300

    update (more if )

--- helper python.py
+++ helper python.py
@@ -177,9 +177,11 @@ with open("text.txt", "w") as textfile:
 
 
 
-
-
-
+"""
+more if
+"""
+def cookie(x):
+    return "Who ate the last cookie? It was %s!" % {str:"Zach", float:"Monica", int:"Monica"}.get(type(x), "the dog")
 
 
 
